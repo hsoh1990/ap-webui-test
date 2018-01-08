50b6a3ff018d174f4f2dfc8e4df8238a28c42197
zxcv1246789 <zxcv1246789@naver.com>
2018-01-08 17:33:18 +0900

    nodejs 연동을 위한 사전 코드 준비

---
+++ server/app.js
@@ -0,0 +1,22 @@
+require('express')
+const express = require('express');
+const router = express.Router();
+
+
+module.exports = function(app, fs)
+{
+
+     app.get('/',function(req,res){
+         res.render('index', {
+             title: "MY HOMEPAGE",
+             length: 5
+         })
+     });
+
+    app.get('/dashboard', function (req, res) {
+       fs.readFile( __dirname + "/data/" + "dashboard.json", 'utf8', function (err, data) {
+           console.log( data );
+           res.end( data );
+       });
+    })
+}
