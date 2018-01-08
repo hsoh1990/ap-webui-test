07586622752ba1d7dc4918a1f6a8cf3f0001ca4d
zxcv1246789 <zxcv1246789@naver.com>
2018-01-08 11:10:47 +0900

    sampledata.js 로 샘플 JSON 데이타 이동 및 정리.

---
+++ sampledata.js
@@ -0,0 +1,32 @@
+<script>
+  function dashboard_data() {
+
+    var dashboardData = {
+      Interface_Information: {
+        "Interface Name": "wlan0",
+        "IP Address": "192.168.0.1",
+        "Subnet Mask": "255.255.255.0",
+        "Mac Address": "b8.27.eb.c6.4f.e3"
+      },
+      Interface_Statistics: {
+        "Received Packets": "No Data",
+        "Received Bytes": "No Data",
+        "Transferred Packets": "No Data",
+        "Transferred Bytes": "No Data"
+      },
+      Wireless_Information: {
+        "Connected To": "Not connected",
+        "AP Mac Address": "",
+        "Bitrate": "",
+        "Signal Level": "",
+        "Transmit Power": "31 dBm",
+        "Frequency": "",
+        "Link Quality": "50"
+      },
+      alert_select: {
+        "Interface is": true
+      }
+    }
+    return dashboardData;
+  }
+</script>
