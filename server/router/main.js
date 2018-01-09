f4b449831dbb92212a7f2f8a1c464c3fe66ce701
zxcv1246789 <zxcv1246789@naver.com>
2018-01-09 14:35:11 +0900

    test - express2

--- server/router/main.js
+++ server/router/main.js
@@ -1,9 +1,29 @@
-module.exports = function(app)
-{
-  app.get('/', function(req,res){
-    res.render('index.html')
-  }),
-  app.get('/about',function(req,res){
-    res.render('about.html');
-  }),
+module.exports = function(app) {
+  app.get('/', function(req, res) {
+    res.render('index.html');
+  });
+  app.get('/dashboard', function(req, res) {
+    res.render('dashboard.html');
+  });
+  app.get('/wificlient', function(req, res) {
+    res.render('wpaconfig.html');
+  });
+  app.get('/hotspot', function(req, res) {
+    res.render('hotspot.html');
+  });
+  app.get('/networking', function(req, res) {
+    res.render('networking.html');
+  });
+  app.get('/dhcpserver', function(req, res) {
+    res.render('dhcpserver.html');
+  });
+  app.get('/auth', function(req, res) {
+    res.render('auth.html');
+  });
+  app.get('/theme', function(req, res) {
+    res.render('changetheme.html');
+  });
+  app.get('/system', function(req, res) {
+    res.render('system.html');
+  });
 }
