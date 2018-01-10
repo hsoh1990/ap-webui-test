f2b88e5e6b9183f2e14da033e39c93ae77c26cf4
zxcv1246789 <zxcv1246789@naver.com>
2018-01-10 16:50:11 +0900

    post - dhcpserver 파트 구현 완료

--- server/router/main.js
+++ server/router/main.js
@@ -6,52 +6,96 @@ module.exports = function(app, fs) {
     res.render('dashboard.html');
   });
   app.get('/api/dashboard', function(req, res) {
-    fs.readFile( __dirname + "/../data/" + "dashboarddata.json", 'utf8', function (err, data) {
-      var dashboarddata = JSON.parse(data);//json text -> json object
+    fs.readFile(__dirname + "/../data/" + "dashboarddata.json", 'utf8', function(err, data) {
+      var dashboarddata = JSON.parse(data); //json text -> json object
       console.log(dashboarddata);
       res.send(dashboarddata);
     })
   });
+
+
   app.get('/wpaconfig', function(req, res) {
     res.render('wpaconfig.html');
   });
   app.get('/api/wpaconfig', function(req, res) {
-    fs.readFile( __dirname + "/../data/" + "wpaconfigdata.json", 'utf8', function (err, data) {
-      var wpaconfigdata = JSON.parse(data);//json text -> json object
+    fs.readFile(__dirname + "/../data/" + "wpaconfigdata.json", 'utf8', function(err, data) {
+      var wpaconfigdata = JSON.parse(data); //json text -> json object
       console.log(wpaconfigdata);
       res.send(wpaconfigdata);
     })
   });
+
+
   app.get('/hotspot', function(req, res) {
     res.render('hotspot.html');
   });
+  app.get('/api/hotspot', function(req, res) {
+    fs.readFile(__dirname + "/../data/" + "hotspotdata.json", 'utf8', function(err, data) {
+      var hotspotdata = JSON.parse(data); //json text -> json object
+      console.log(hotspotdata);
+      res.send(hotspotdata);
+    })
+  });
+
   app.get('/networking', function(req, res) {
     res.render('networking.html');
   });
   app.get('/api/networking', function(req, res) {
-    fs.readFile( __dirname + "/../data/" + "networkingdata.json", 'utf8', function (err, data) {
-      var wpaconfigdata = JSON.parse(data);//json text -> json object
+    fs.readFile(__dirname + "/../data/" + "networkingdata.json", 'utf8', function(err, data) {
+      var wpaconfigdata = JSON.parse(data); //json text -> json object
       console.log(wpaconfigdata);
       res.send(wpaconfigdata);
     })
   });
+
+
   app.get('/dhcpserver', function(req, res) {
     res.render('dhcpserver.html');
   });
   app.get('/api/dhcpserver', function(req, res) {
-    fs.readFile( __dirname + "/../data/" + "dhcpserverdata.json", 'utf8', function (err, data) {
-      var dhcpserverdata = JSON.parse(data);//json text -> json object
+    fs.readFile(__dirname + "/../data/dhcpserver/" + "clientlist.json", 'utf8', function(err, data) {
+      var dhcpserverdata = JSON.parse(data); //json text -> json object
       console.log(dhcpserverdata);
       res.send(dhcpserverdata);
     })
   });
+  app.post('/api/dhcpserver/send', function(req, res) {
+    req.accepts('application/json');
+    // input message handling
+    var data_name = "serversetting_data";
+    json = req.body;
+
+    console.log('interface :' + json.interface);
+    console.log('starting_IP_address :' + json.starting_IP_address);
+    // output message
+    fs.writeFile(__dirname + "/../data/dhcpserver/" + "serversetting.json",
+      JSON.stringify(json, null, '\t'), "utf8",
+      function(err, data) {
+        result = {
+          "success": 1
+        };
+        res.json(result);
+      })
+  });
+
   app.get('/auth', function(req, res) {
     res.render('auth.html');
   });
+
+
   app.get('/changetheme', function(req, res) {
     res.render('changetheme.html');
   });
+
+
   app.get('/system', function(req, res) {
     res.render('system.html');
+  });
+  app.get('/api/system', function(req, res) {
+    fs.readFile(__dirname + "/../data/" + "systeminfordata.json", 'utf8', function(err, data) {
+      var systeminfordata = JSON.parse(data); //json text -> json object
+      console.log(systeminfordata);
+      res.send(systeminfordata);
+    })
   });
 }
--- server/server.js
+++ server/server.js
@@ -4,7 +4,6 @@ var bodyParser = require('body-parser');
 var session = require('express-session');
 var fs = require("fs");
 
-console.log(__dirname)
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
@@ -13,9 +12,9 @@ var server = app.listen(80, function() {
   console.log("Express server has started on port 80");
 });
 
+app.use(express.json());
 app.use(express.static('public'));
 app.use(express.static('views'));
-
 app.use(bodyParser.urlencoded({
   extended: true
 }));
