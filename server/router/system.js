3486058a2f724889eec5cf6ee4e2e8893c8d24ed
zxcv1246789 <zxcv1246789@naver.com>
2018-01-12 16:45:00 +0900

    v 0.0.2

--- server/router/system.js
+++ server/router/system.js
@@ -3,7 +3,7 @@ var fs = require("fs");
 exports.api_get = function (req, res) {
   fs.readFile(__dirname + "/../data/" + "systeminfordata.json", 'utf8', function(err, data) {
     var systeminfordata = JSON.parse(data); //json text -> json object
-    console.log(systeminfordata);
+    //console.log(systeminfordata);
     res.send(systeminfordata);
   })
 }
