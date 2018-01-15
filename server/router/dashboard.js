3123b881a742e803ddbcd712c282d7a0f1382f5b
zxcv1246789 <zxcv1246789@naver.com>
2018-01-15 01:54:37 +0000

    test - dashboard.js

--- server/router/dashboard.js
+++ server/router/dashboard.js
@@ -1,9 +1,23 @@
 var fs = require("fs");
+var exec = require('child_process').exec,
+    child;
 
 exports.api_get = function (req, res) {
   fs.readFile(__dirname + "/../data/" + "dashboarddata.json", 'utf8', function(err, data) {
     var dashboarddata = JSON.parse(data); //json text -> json object
-    //console.log(dashboarddata);
+    var execresult;
+//console.log(dashboarddata);
+    child = exec("ip a s eth0", function (error, stdout, stderr) {
+    console.log('stdout: ' + stdout);
+    stdout = stdout.replace(/\s\s+/,' ');
+    console.log('replace: ' + stdout);
+    stdout = stdout.match(/link\/ether ([0-9a-f:]+)/i);
+    console.log('mac: ' + stdout[1]);
+    console.log('stderr: ' + stderr);
+        if (error !== null) {
+            console.log('exec error: ' + error);
+        }
+    });
     res.send(dashboarddata);
   })
 }
