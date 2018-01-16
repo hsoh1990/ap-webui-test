3d3047069d72e4e0e040301f11448f48759c87c3
zxcv1246789 <zxcv1246789@naver.com>
2018-01-16 16:37:40 +0900

    test

--- server/router/dashboard.js
+++ server/router/dashboard.js
@@ -10,11 +10,12 @@ exports.api_get = function (req, res) {
       console.log('text: ' + text);
       stdout = text.replace(/\s\s+/,' ');
       console.log('replace: ' + text);
-
+      child = exec("ifconfig wlan0", function (error, stdout2, stderr) {});
+      console.log('ifconfig wlan0: ' + child);
       var ip = text.match(/inet ([0-9.]+)/i);
       console.log('ip: ' + ip[1]);
       var netmask = text.match(/[0-9.]+\/([0-3][0-9])/i);
-      console.log('mac: ' + netmask[1]);
+      console.log('netmask: ' + netmask[1]);
       var mac = text.match(/link\/ether ([0-9a-f:]+)/i);
       console.log('mac: ' + mac[1]);
 
