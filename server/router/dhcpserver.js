6a2fd2e5408cf944e487f475adf8db7f225ee00b
zxcv1246789 <zxcv1246789@naver.com>
2018-01-16 16:07:10 +0900

    test

--- server/router/dhcpserver.js
+++ server/router/dhcpserver.js
@@ -7,7 +7,7 @@ exports.api_get = function (req, res) {
     var dhcpserverdata = JSON.parse(data); //json text -> json object
     //console.log(dhcpserverdata);
     child = exec("cat /var/lib/misc/dnsmasq.leases", function (error, stdout, stderr) {
-    console.log('stdout: ' + stdout[1]);
+    console.log('stdout: ' + stdout[0][1]);
     var arr = [];
 
     /*for (var i = 0;i < stdout.length;i++){
