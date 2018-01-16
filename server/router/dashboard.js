0715756c16e9484bcd76eb5726daa1784318b118
zxcv1246789 <zxcv1246789@naver.com>
2018-01-16 16:56:15 +0900

    test

--- server/router/dashboard.js
+++ server/router/dashboard.js
@@ -1,15 +1,15 @@
 var fs = require("fs");
 var exec = require('child_process').exec,
-    child;
+  child;
 
-exports.api_get = function (req, res) {
+exports.api_get = function(req, res) {
 
-  child = exec("ip a s wlan0", function (error, stdout1, stderr) {
-    child = exec("iwconfig wlan0", function (error, stdout2, stderr) {
-      child = exec("ifconfig wlan0", function (error, stdout3, stderr) {
+  child = exec("ip a s wlan0", function(error, stdout1, stderr) {
+    child = exec("iwconfig wlan0", function(error, stdout2, stderr) {
+      child = exec("ifconfig wlan0", function(error, stdout3, stderr) {
         var text = stdout1 + stdout2;
         console.log('text: ' + text);
-        stdout = text.replace(/\s\s+/,' ');
+        stdout = text.replace(/\s\s+/, ' ');
         console.log('replace: ' + text);
 
         var ip = text.match(/inet ([0-9.]+)/i);
@@ -19,26 +19,28 @@ exports.api_get = function (req, res) {
         var mac = text.match(/link\/ether ([0-9a-f:]+)/i);
         console.log('mac: ' + mac[1]);
         var RX_packets = text.match(/RX packets:(\d+)/);
-        if (RX_packets[1] == null) {
-          console.log('RX_packets: No Data');
-        }
-        else {
-          console.log('RX_packets: ' + RX_packets[1]);
+        try {
+          if (RX_packets[1] != null) {
+            console.log('RX_packets: ' + RX_packets[1]);
+          } else {
+            throw "RX_packets: No Data";
+          }
+        } catch (e) {
+          console.log(e);
         }
 
         var numReturn = text.indexOf("UP");
-        if (numReturn != -1){
+        if (numReturn != -1) {
           console.log('indexOf: UP');
-        }
-        else {
+        } else {
           console.log('indexOf: DOWN');
         }
         console.log('indexOf: ' + numReturn);
 
         console.log('stderr: ' + stderr);
-            if (error !== null) {
-                console.log('exec error: ' + error);
-            }
+        if (error !== null) {
+          console.log('exec error: ' + error);
+        }
       });
     });
   });
@@ -46,7 +48,7 @@ exports.api_get = function (req, res) {
   fs.readFile(__dirname + "/../data/" + "dashboarddata.json", 'utf8', function(err, data) {
     var dashboarddata = JSON.parse(data); //json text -> json object
     var execresult;
-//console.log(dashboarddata);
+    //console.log(dashboarddata);
     res.send(dashboarddata);
   })
 }
