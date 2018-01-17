919e80af48149e11e78c384545cddf5344066a42
zxcv1246789 <zxcv1246789@naver.com>
2018-01-17 11:46:12 +0900

    test

--- server/router/dashboard.js
+++ server/router/dashboard.js
@@ -43,7 +43,7 @@ exports.consolelog_serverdata = function () {
         fs.readFile(__dirname + "/../data/" + "dashboarddata.json", 'utf8', function(err, data) {
           var boarddata = JSON.parse(data);//텍스트 -> 오브젝트
           // ADD TO DATA
-          console.log('boarddata: ' + data);
+          console.log('boarddata: ' + data["Interface_Information"]);
 
           // SAVE DATA
           /*fs.writeFile(__dirname + "/../data/" + "dashboarddata.json",
