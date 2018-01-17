de54b75db05c95df91828ce556c9971411bdca0b
zxcv1246789 <zxcv1246789@naver.com>
2018-01-17 13:11:39 +0900

    test

--- server/router/dashboard.js
+++ server/router/dashboard.js
@@ -42,7 +42,7 @@ exports.consolelog_serverdata = function() {
 
         // ADD TO DATA
         var Interface_Information = {}; //오브젝트
-        Interface_Information["Interface Name"] = "wlan1";
+        Interface_Information["Interface Name"] = "wlan0";
         Interface_Information["IP Address"] = ip;
         Interface_Information["Subnet Mask"] = netmask;
         Interface_Information["Mac Address"] = mac;
