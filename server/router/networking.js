3c1c093baf73b06b5028e2e9bedd89cbea350bfd
zxcv1246789 <zxcv1246789@naver.com>
2018-01-18 15:22:22 +0900

    test

--- server/router/networking.js
+++ server/router/networking.js
@@ -3,46 +3,29 @@ var exec = require('child_process').exec,
   child;
 
 exports.api_get = function(req, res) {
-  exports.savedata_();
+  var arr = exports.savedata_1();
+  exports.savedata_2(arr[0]);
   fs.readFile(__dirname + "/../data/networking/" + "summary.json", 'utf8', function(err, data) {
     var wpaconfigdata = JSON.parse(data); //json text -> json object
     //console.log(wpaconfigdata);
     res.send(wpaconfigdata);
   })
 }
-exports.savedata_ = function() {
+exports.savedata_1 = function() {
   child = exec("ls /sys/class/net | grep -v lo", function(error, stdout1, stderr) {
     var arr = stdout1.split("\n");
-    var cs_data = {}; //오브젝트
-    for (var a = 0; a < arr.length; a++) {
-      cs_data[a] = arr[a];
-    }
-    var count = 0;
+    return arr;
+  });
+}
+exports.savedata_2 = function(text) {
+  child = exec("ip a show " + text, function(error, stdout2, stderr) {
+    var eth = {};
     var result_data = {};
-    console.log("1 : " + arr[0]);
-    for (var i = 0; i < arr.length - 1; i++) {
-      console.log("for : " + arr[i]);
-      child = exec("ip a show " + arr[i], function(error, stdout2, stderr) {
-        child = exec("ls /sys/class/net | grep -v lo", function(error, stdout3, stderr) {
-          var arr_ = stdout3.split("\n");
-          var eth = {};
-          console.log("eth : " + arr_[i]);
-          eth[arr_[i]] = stdout2.replace(/\n/gi, "<br>");
-          var eng = "current_setting_" + arr_[i];
-          result_data[eng] = eth;
-          console.log("----------------------------------------------------");
-        });
-
-      });
-    }
-    console.log("! == " + JSON.stringify(result_data));
-    fs.writeFile(__dirname + "/../data/networking/" + "summary.json",
-      JSON.stringify(result_data, null, '\t'), "utf8",
-      function(err, data) {
-        result = {
-          "success": 1
-        };
-      })
+    eth[text] = stdout2.replace(/\n/gi, "<br>");
+    console.log("eth : " + text);
+    var eng = "current_setting_" + text;
+    result_data[eng] = eth;
+    console.log("eth : " + result_data);
   });
 }
 exports.api_post = function(req, res) {
