0b0ddf8e7fc5b8a47393c19074d045871b3c286b
zxcv1246789 <zxcv1246789@naver.com>
2018-01-18 15:12:36 +0900

    test

--- server/router/networking.js
+++ server/router/networking.js
@@ -1,8 +1,8 @@
 var fs = require("fs");
 var exec = require('child_process').exec,
-    child;
+  child;
 
-exports.api_get = function (req, res) {
+exports.api_get = function(req, res) {
   exports.savedata_();
   fs.readFile(__dirname + "/../data/networking/" + "summary.json", 'utf8', function(err, data) {
     var wpaconfigdata = JSON.parse(data); //json text -> json object
@@ -10,25 +10,29 @@ exports.api_get = function (req, res) {
     res.send(wpaconfigdata);
   })
 }
-exports.savedata_ = function () {
-  child = exec("ls /sys/class/net | grep -v lo", function (error, stdout1, stderr) {
+exports.savedata_ = function() {
+  child = exec("ls /sys/class/net | grep -v lo", function(error, stdout1, stderr) {
     var arr = stdout1.split("\n");
-    var cs_data = {};//오브젝트
-    for(var a = 0;a < arr.length;a++){
+    var cs_data = {}; //오브젝트
+    for (var a = 0; a < arr.length; a++) {
       cs_data[a] = arr[a];
     }
     var count = 0;
     var result_data = {};
-    console.log("1 : "+arr[0]);
-    for(var i = 0;i < arr.length - 1;i++){
-      console.log("for : "+arr[i]);
-      child = exec("ip a show " + arr[i], function (error, stdout2, stderr) {
-        var eth = {};
-        console.log("eth : "+arr[i]);
-        eth[arr[i]] = stdout2.replace(/\n/gi, "<br>");
-        var eng = "current_setting_" + arr[i];
-        result_data[eng] = eth;
-        console.log("----------------------------------------------------");
+    console.log("1 : " + arr[0]);
+    for (var i = 0; i < arr.length - 1; i++) {
+      console.log("for : " + arr[i]);
+      child = exec("ip a show " + arr[i], function(error, stdout2, stderr) {
+        child = exec("ls /sys/class/net | grep -v lo", function(error, stdout1, stderr) {
+          var arr = stdout1.split("\n");
+          var eth = {};
+          console.log("eth : " + arr[i]);
+          eth[arr[i]] = stdout2.replace(/\n/gi, "<br>");
+          var eng = "current_setting_" + arr[i];
+          result_data[eng] = eth;
+          console.log("----------------------------------------------------");
+        });
+
       });
     }
     console.log("! == " + JSON.stringify(result_data));
@@ -41,7 +45,7 @@ exports.savedata_ = function () {
       })
   });
 }
-exports.api_post = function (req, res) {
+exports.api_post = function(req, res) {
   req.accepts('application/json');
   // input message handling
   var result = {};
@@ -49,7 +53,7 @@ exports.api_post = function (req, res) {
   json = req.body;
   exports.api_post_datasave(req, res, adapt_name, result);
 }
-exports.api_post_datasave = function (req, res, adapt_name, result) {
+exports.api_post_datasave = function(req, res, adapt_name, result) {
   // output message
   fs.readFile(__dirname + "/../data/networking/" + "connecting_lan_data.json", 'utf8', function(err, data) {
     var users = JSON.parse(data);
