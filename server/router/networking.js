f527558d79ad1c0c79eadb83ebe6c6c4ecfaa846
zxcv1246789 <zxcv1246789@naver.com>
2018-01-18 13:13:09 +0900

    test

--- server/router/networking.js
+++ server/router/networking.js
@@ -1,8 +1,9 @@
 var fs = require("fs");
 var exec = require('child_process').exec,
     child;
- 
+
 exports.api_get = function (req, res) {
+  exports.savedata_();
   fs.readFile(__dirname + "/../data/networking/" + "summary.json", 'utf8', function(err, data) {
     var wpaconfigdata = JSON.parse(data); //json text -> json object
     //console.log(wpaconfigdata);
@@ -13,7 +14,23 @@ exports.api_get = function (req, res) {
     res.send(wpaconfigdata);
   })
 }
+exports.savedata_ = function () {
+  child = exec("ls /sys/class/net | grep -v lo", function (error, stdout1, stderr) {
+    var arr = stdout.split("\n");
+    var cs_data = {};//오브젝트
+    for(var a = 0;a < arr.length;a++){
+      cs_data[a] = arr[a];
+    }
+    child = exec("ip a show " + arr[0], function (error, stdout2, stderr) {
+      var eth = {};
+      eth['eth0'] = stdout2;
+      var result_data = {};
+      result_data['current_setting_eth0'] = eth;
+      console.log(result_data);
+    });
+  });
 
+}
 exports.api_post = function (req, res) {
   req.accepts('application/json');
   // input message handling
