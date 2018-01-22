38319532cb082f31f57d06f901fdd000557d74b2
zxcv1246789 <zxcv1246789@naver.com>
2018-01-22 09:59:16 +0900

    test

--- server/router/system.js
+++ server/router/system.js
@@ -36,8 +36,8 @@ exports.api_get = function(req, res) {
   const std_mem = execSync('free -m | awk /Mem:/', {
     encoding: 'utf8'
   });
-  var mem__ = std_mem.match(/[0-9]\d*/i);
-  console.log("result = \n" + mem__);
+  var mem__ = std_mem.match(/\S[0-9]\d*+/i);
+  console.log("result = " + mem__);
   fs.readFile(__dirname + "/../data/" + "systeminfordata.json", 'utf8', function(err, data) {
     var systeminfordata = JSON.parse(data); //json text -> json object
     //console.log(systeminfordata);
