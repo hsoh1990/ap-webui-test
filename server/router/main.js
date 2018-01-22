a51a64a72a1af507b86a266bc63336d8a402be47
zxcv1246789 <zxcv1246789@naver.com>
2018-01-22 14:10:22 +0900

    test

--- server/router/main.js
+++ server/router/main.js
@@ -62,9 +62,9 @@ module.exports = function(app, fs, url) {
       router_dashboard.api_get(req, res);
     } else if (type == "wlan0stopstart") {
       if (select == 0) {//stop 시키는 부분
-
+        router_dashboard.wlan0_stop(req, res);
       } else if (select == 1) {//start 시키는 부분
-
+        router_dashboard.wlan0_start(req, res);
       }
       else {
         router_dashboard.start_stopbutton(req, res);
