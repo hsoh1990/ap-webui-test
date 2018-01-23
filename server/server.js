59a2cb50318b9fd95ab700c4dc6f4a1b4e66d751
zxcv1246789 <zxcv1246789@naver.com>
2018-01-24 00:42:43 +0900

    test

--- server/server.js
+++ server/server.js
@@ -22,7 +22,7 @@ app.use(express.static('public'));
 app.use(express.static('views'));
 app.use(bodyParser.urlencoded({
   extended: true
-}));t
+}));
 app.use(bodyParser.json());
 app.use(session({
   secret: '@#@$MYSIGN#@$#$',
