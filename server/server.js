703604f9cf4c86ddc335bc91d5804778c7860df1
zxcv1246789 <zxcv1246789@naver.com>
2018-01-24 01:05:19 +0900

    test

--- server/server.js
+++ server/server.js
@@ -10,8 +10,6 @@ var exec = require('child_process').exec,
   child;
 
 app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-app.engine('html', require('ejs').renderFile);
 
 var server = app.listen(80, function() {
   console.log("Express server has started on port 80");
