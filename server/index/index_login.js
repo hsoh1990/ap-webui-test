52a3670c4f97529aefe4383970f16824d109107f
zxcv1246789 <zxcv1246789@naver.com>
2018-01-25 15:04:53 +0900

    test

--- server/index/index_login.js
+++ server/index/index_login.js
@@ -1,14 +1,12 @@
 var fs = require("fs");
 var exec = require('child_process').exec,
   child;
-  const {
-    execSync
-  } = require('child_process');
+const {
+  execSync
+} = require('child_process');
 
 exports.sidemenu_get = function(req, res) {
   fs.readdir('../package/', (err, files) => {
-  files.forEach(file => {
-    console.log(file);
-  });
-})
+    console.log(files);
+  })
 }
