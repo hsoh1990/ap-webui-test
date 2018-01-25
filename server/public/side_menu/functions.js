80efe8be5c6969973ea5879c5d47fd038508a630
zxcv1246789 <zxcv1246789@naver.com>
2018-01-25 16:11:50 +0900

    test

---
+++ server/public/side_menu/functions.js
@@ -0,0 +1,29 @@
+function sidemenu_get() { //사이드 메뉴 데이터를 받아옵니다
+  const xhr = new XMLHttpRequest();
+  // by default async
+  xhr.open("GET", "/api/index_login?type=sidemenu", true);
+  xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+  xhr.responseType = 'json';
+
+  xhr.onload = function() {
+    if (this.readyState == 4 && this.status == 200) { // onload called even on 404 etc so check the status
+      //alert("전송 결과 메시지 : " + JSON.stringify(this.response));
+      sidemenu_output(this.response);
+    }
+  };
+  xhr.onerror = function() {
+    console.log("confirm");
+  };
+  xhr.send();
+}
+
+function sidemenu_output(sidemenu_data) {
+  var sidemenu_data_key = Object.getOwnPropertyNames(sidemenu_data);
+  var content = "";
+  for (var a = 0; a < Object.keys(sidemenu_data).length; a++) {
+    content += "<li>";
+    content += "<a href=" + sidemenu_data_key[a] + "><i class=''></i> " + sidemenu_data[sidemenu_data_key[a]] + "</a>"
+    content += "</li>";
+  }
+  document.getElementById("side-menu").innerHTML = content;
+}
