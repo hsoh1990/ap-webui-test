230f1ea97f030aab4a9e09ecca4c4d7b27a2ccce
zxcv1246789 <zxcv1246789@naver.com>
2018-01-25 22:12:02 +0900

    test

---
+++ server/index/main.js
@@ -0,0 +1,45 @@
+module.exports = function(app, fs, url) {
+  var router_index_login = require('./index_login.js');
+
+  app.get('/', function(req, res) {
+    res.render('index.html');
+  });
+  app.get('/index_login', function(req, res) {
+    var sess;
+    sess = req.session;
+    console.log('session : ' + sess.logincheck);
+    if (sess.logincheck == "1") {
+      res.render('index_login.html');
+    } else {
+      res.render('index.html');
+    }
+  });
+  app.get('/api/index_login', function(req, res) {
+    req.accepts('application/json');
+    // input message handling
+    var type = req.query.type;
+
+    if (type == "sidemenu") {
+      router_index_login.sidemenu_get(req, res);
+    }
+  });
+
+  app.get('/login_check', function(req, res) {
+    var sess;
+    sess = req.session;
+    var id = req.query.id;
+    var password = req.query.password;
+    fs.readFile(__dirname + "/userdata/" + "userdata.json", 'utf8', function(err, data) {
+      var userdata = JSON.parse(data); //json text -> json object
+      var check = {};
+      if (id == userdata['admin']['username'] && password == userdata['admin']['password']) {
+        sess.logincheck = "1";
+      } else {
+        sess.logincheck = "0";
+      }
+      check['check'] = sess.logincheck;
+      console.log('session : ' + sess.logincheck);
+      res.send(check);
+    })
+  });
+}
---
+++ server/package/system/main.js
@@ -0,0 +1,28 @@
+module.exports = function(app, fs, url){
+	var router_system = require('./system.js');
+
+	app.get('/system', function(req, res) {
+    var sess;
+    sess = req.session;
+    console.log('session : ' + sess.logincheck);
+    if (sess.logincheck == "1") {
+      res.render('system.html');
+    } else {
+      res.render('index.html');
+    }
+  });
+  app.get('/api/system', function(req, res) {
+    req.accepts('application/json');
+    // input message handling
+    var type = req.query.type;
+    if (type == "refresh") {
+      router_system.api_get(req, res);
+    } else if (type == "reboot") {
+      router_system.system_reboot(req, res);
+    } else if (type == "shutdown") {
+      router_system.system_shutdown(req, res);
+    } else if (type == "package") {
+      router_system.package_data_get(req, res);
+    }
+  });
+};
