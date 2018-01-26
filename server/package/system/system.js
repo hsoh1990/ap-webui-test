990a9c77895a81809ecb11aff809b919bc23a9b7
zxcv1246789 <zxcv1246789@naver.com>
2018-01-26 10:40:42 +0900

    test

--- server/package/system/system.js
+++ server/package/system/system.js
@@ -181,7 +181,7 @@ exports.uninstall_package = function(req, res, select) {
   for (var i = 0; i < files.length; i++) {
     if (select == i){
       var package_name = files[i];
-      execSync('sudo zip ' + __dirname + '/../../package_tmp/' + package_name + '.zip ../' + package_name + '/*', {
+      execSync('sudo zip ' + __dirname + '/../../package_tmp/' + package_name + '.zip '+ __dirname +'../' + package_name + '/*', {
         encoding: 'utf8'
       });
       fs.rmdirSync(__dirname + '/../' + package_name)
