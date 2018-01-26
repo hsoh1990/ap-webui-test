37b338f320c9f9b6f896bd05587e7974c9b43e77
zxcv1246789 <zxcv1246789@naver.com>
2018-01-26 16:42:07 +0900

    test

--- server/package/system/system.js
+++ server/package/system/system.js
@@ -220,10 +220,10 @@ exports.install_package = function(req, res, select) {
       console.log("serverjs_data : \n" + serverjs_data);
       var data_split = serverjs_data.split("\n");
       console.log("data_split : \n" + data_split[0]);
+      var insert_data = "  __dirname + '"'"/package/default"'"',";
+      data_split.splice(15,0,insert_data);
 
-
-
-
+      console.log("data_split : \n" + data_split);
 
 
 
