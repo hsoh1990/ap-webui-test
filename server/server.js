466b8444d77d34544900a5370267661628b77300
zxcv1246789 <zxcv1246789@naver.com>
2018-01-30 10:03:08 +0900

    test

--- server/server.js
+++ server/server.js
@@ -1,23 +1,15 @@
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
-var session = require('express-session');
-var fs = require("fs");
-var http = require('http');
-var url = require('url');
-var querystring = require('querystring');
-var exec = require('child_process').exec,
-  child;
-var path = require('path');
+module.exports = function(app, fs, url) {
 
 
-app.set('views', [
-  __dirname + '/index',
-  __dirname + '/package/dashboard',
-  __dirname + '/package/system',
-]);
+  app.set('views', [
+    __dirname + '/index',
+    __dirname + '/package/dashboard',
+    __dirname + '/package/system',
+  ]);
 
 
-require('./index/main.js')(app, fs, url);
-require('./package/dashboard/main.js')(app, fs, url);
-require('./package/system/main.js')(app, fs, url);
+  require('./index/main.js')(app, fs, url);
+  require('./package/dashboard/main.js')(app, fs, url);
+  require('./package/system/main.js')(app, fs, url);
+
+}
