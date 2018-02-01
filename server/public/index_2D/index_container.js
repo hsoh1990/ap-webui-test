4322f7fce6697fa0ec20ea8d1df50fa70b7bf315
zxcv1246789 <zxcv1246789@naver.com>
2018-02-01 16:57:31 +0900

    test

--- server/public/index_2D/index_container.js
+++ server/public/index_2D/index_container.js
@@ -56,7 +56,7 @@ var Line4 = new Konva.Line({//우측위 큰선
 var sin_y = 200 * Math.sin(30 * Math.PI/180);
 
 var simpleText = new Konva.Text({
-      x: stage.getWidth() / 2,
+      x: 15,
       y: 15,
       text: String(sin_y),
       fontSize: 30,
