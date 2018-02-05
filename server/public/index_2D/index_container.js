3d47f467cec35dac3c293fea70423477a4a4999a
zxcv1246789 <zxcv1246789@naver.com>
2018-02-05 15:28:31 +0900

    test

--- server/public/index_2D/index_container.js
+++ server/public/index_2D/index_container.js
@@ -7,6 +7,7 @@ var stage = new Konva.Stage({
 
 var aplayer = new Konva.Layer();
 var textlayer = new Konva.Layer();
+var wlanlayer = new Konva.Layer();
 
 var connect_radius = 380;
 var disconnect_radius = 550;
@@ -81,10 +82,12 @@ function connect_AP() {
 }
 //ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
 function wlan_draw() {
-  var wlan_x = stage.getWidth() / 2
+  var wlan_x = stage.getWidth() / 2 - 40 - 300;
+  var wlan_y = stage.getHeight() / 2 - 15;
+
   var wlan_box = new Konva.Rect({
-    x: x,
-    y: y,
+    x: wlan_x,
+    y: wlan_y,
     width: 40,
     height: 30,
     fill: 'gray',
@@ -92,18 +95,19 @@ function wlan_draw() {
     strokeWidth: 3
   });
 
-  var x = stage.getWidth() / 2 - 40 + resultxy[a][0];
-  var y = stage.getHeight() / 2 - 30 + resultxy[a][1];
-
   var wlan_line = new Konva.Line({
-    points: [stage.getWidth() / 2, stage.getHeight() / 2, x + 20, y + 15],
+    points: [stage.getWidth() / 2, stage.getHeight() / 2, wlan_x + 20, wlan_y + 15],
     stroke: 'black',
     strokeWidth: 3,
     lineCap: 'round',
     lineJoin: 'round'
   });
 
+  wlanlayer.add(wlan_box);
+  wlanlayer.add(wlan_line);
+  stage.add(wlanlayer);
 }
+
 function disconnect_draw(res_count, conn_count) {
 
 
