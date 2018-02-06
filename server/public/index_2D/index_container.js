11073edad3b5c72f239dddd5b6ab76d4a60756db
zxcv1246789 <zxcv1246789@naver.com>
2018-02-06 17:13:12 +0900

    test

--- server/public/index_2D/index_container.js
+++ server/public/index_2D/index_container.js
@@ -46,7 +46,7 @@ function connect_AP(ap_data) {
 
   var ap_text = ap_data[0]['IP Address'] + "\n" + ap_data[0]['Host name'];
   var complexText = new Konva.Text({
-      x: stage.getWidth() / 2 - 80 - AP_Rect.getWidth(),
+      x: stage.getWidth() / 2 - 70 - AP_Rect.getWidth(),
       y: stage.getHeight() / 2 - 30 + 55,
       text: ap_text,
       fontSize: 18,
