7a77ff90665cd12b46d6219766a846fb52f63eb5
zxcv1246789 <zxcv1246789@naver.com>
2018-02-08 17:14:25 +0900

    test

--- server/public/index_2D/index_container.js
+++ server/public/index_2D/index_container.js
@@ -14,7 +14,7 @@ function addJavascript(jsname) {
 }
 var path = window.location.pathname;
 
-addJavascript(path + '/socket.io.js');
+addJavascript(path + '../socket.io.js');
 
 socket_ = io.connect('http://172.16.171.181:8080');
 
