a4619203e7daa46fd4c26d9b30640d150986669a
zxcv1246789 <zxcv1246789@naver.com>
2018-02-09 12:54:34 +0900

    test

--- server/index/main.js
+++ server/index/main.js
@@ -64,7 +64,8 @@ module.exports = function(app, fs, url) {
 
   var sockets = new Array();
   var arp_count = 0;
-  var device_data = fs.readFileSync(__dirname + "/data/device_data.json", 'utf8');
+  var device_data = new Array();
+  device_data = fs.readFileSync(__dirname + "/data/device_data.json", 'utf8');
 
   ! function arp_repeat() {
     arp_count++;
