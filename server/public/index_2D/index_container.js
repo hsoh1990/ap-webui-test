128d1aac20a9341a3dd236e587a30a74c43b4da7
zxcv1246789 <zxcv1246789@naver.com>
2018-02-09 09:51:23 +0900

    test

--- server/public/index_2D/index_container.js
+++ server/public/index_2D/index_container.js
@@ -320,7 +320,7 @@ function wlan_draw(enable__, wlan_data) {
   }
 
   var wlantext = new Konva.Text({
-    x: wlan_x - 100 - wlan_box.getWidth(),
+    x: wlan_x - 140 - wlan_box.getWidth(),
     y: wlan_y + 25,
     text: wlan_text,
     fontSize: 18,
