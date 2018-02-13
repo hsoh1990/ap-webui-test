8f2edc4f53d3597d53ac4ee8c5ff4c0ff0534926
zxcv1246789 <zxcv1246789@naver.com>
2018-02-13 11:27:53 +0900

    test

--- server/public/index_2D/index_container.js
+++ server/public/index_2D/index_container.js
@@ -631,8 +631,8 @@ function disconnect_draw(enable, res_count, conn_count) {
     });
 
     var owner_text = new Konva.Text({
-      x: x - 145,
-      y: y - 80,
+      x: x - 140,
+      y: y - 90,
       text: res_count[a]['owner'],
       fontSize: 18,
       fontFamily: 'Calibri',
