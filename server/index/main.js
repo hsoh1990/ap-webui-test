dfeb80a5c6c5e55939032e9ef2ea03c87b8ac57d
zxcv1246789 <zxcv1246789@naver.com>
2018-02-13 11:01:43 +0900

    test

--- server/index/main.js
+++ server/index/main.js
@@ -226,7 +226,7 @@ module.exports = function(app, fs, url) {
     socket.on('owner__disconnect', function(data) {
       var stringify_data = fs.readFileSync(__dirname + "/data/device_data.json", 'utf8');
       var parse_data = JSON.parse(stringify_data);
-      console.log(parse_data);
+      console.log(parse_data + ",,, " + data['mac']);
       for (var a = 0;a < parse_data.lrngth; a++) {
         if(parse_data[a]['MAC Address'] == data['mac']) {
           parse_data[a]['owner'] = data['owner'];
