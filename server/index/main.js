6aaa0058e0b7e822f21963759f428ce6debb9f76
zxcv1246789 <zxcv1246789@naver.com>
2018-02-14 09:54:45 +0900

    test

--- server/index/main.js
+++ server/index/main.js
@@ -128,11 +128,12 @@ module.exports = function(app, fs, url) {
     var data_key = Object.getOwnPropertyNames(data__);
     var tmp_ind = device_data.length;
     console.log("길이 : " + Object.keys(data__).length);
-    for (var a = 0; a < tmp_ind; a++) {
+    var count = 0;
+    for (count; count < tmp_ind; ++count) {
       if (device_data[a]['length'] != Object.keys(data__).length) {
         console.log("삭제함.");
         device_data.splice(a, 1);
-        a -= 1;
+        count -= 1;
       }
     }
     for (var a = 0; a < Object.keys(data__).length; a++) {
