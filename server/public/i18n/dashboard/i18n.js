e780a9d386443bb5ed659ffaecba20fe3eecc4ac
zxcv1246789 <zxcv1246789@naver.com>
2018-02-14 10:52:14 +0900

    test

--- server/public/i18n/dashboard/i18n.js
+++ server/public/i18n/dashboard/i18n.js
@@ -29,7 +29,7 @@ function updateContent_nav() {
     document.getElementById('dashboard_navid').innerHTML = i18next.t('name');
   }
   var contentData = window.document.getElementsByClassName("nav");
-  var child_li = contentData..getElementsByTagName("li");
+  var child_li = contentData.getElementsByTagName("li");
   alert("자식 노드 개수는? " + contentData.length);
 
 }
