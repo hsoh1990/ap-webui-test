556d284e501b9685a953c9fb8102af010a8daf4b
zxcv1246789 <zxcv1246789@naver.com>
2018-02-19 16:50:13 +0900

    test

--- server/public/system_js/package.js
+++ server/public/system_js/package.js
@@ -32,7 +32,7 @@ function package_output(package_data) {
     if (package_data_key[a] == "System" || package_data_key[a] == "Dashboard") {
 
     } else {
-      content += "<td><input type=button class='btn btn-danger' onclick='uninstall_button(" + a + ")' id=uninstall_id ></td>";
+      content += "<td><input type=button class='btn btn-danger' onclick='uninstall_button(" + a + ")' id=uninstall_id value=\"\" /></td>";
     }
     content += "</tr>";
   }
@@ -88,7 +88,7 @@ function install_output(install_data) {
     content += "<td>" + install_data[install_data_key[a]]['pack_name'] + "</td>";
     content += "<td> </td>";
     content += "<td> </td>";
-    content += "<td><input type=button class='btn btn-primary' onclick='install_button(" + a + ")' id=install_id ></td>";
+    content += "<td><input type=button class='btn btn-primary' onclick='install_button(" + a + ")' id=install_id value=\"\" /></td>";
     content += "</tr>";
   }
   document.getElementById("package_no_install").innerHTML = content;
