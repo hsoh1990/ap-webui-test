59fde6568a7fdfea3da5a222a8348a4cbaf7712b
zxcv1246789 <zxcv1246789@naver.com>
2018-02-19 16:38:05 +0900

    test

--- server/public/i18n/system/i18n.js
+++ server/public/i18n/system/i18n.js
@@ -98,8 +98,8 @@ function updateContent_uninstallbtn() {
     var child_td = child_tr[a].getElementsByTagName("td");
     for (var b = 0; b < child_td.length; b++) {
       if (child_td[b].getElementsByTagName("button") != null) {
-        var btn__ = child_td[b].getElementById("uninstall_id");
-        btn__.innerHTML = i18next.t('uninstall_btn');
+        var btn__ = child_td[b].getElementsByTagName("button");
+        btn__[0].innerHTML = i18next.t('uninstall_btn');
       }
     }
   }
@@ -112,8 +112,8 @@ function updateContent_installbtn() {
     var child_td = child_tr[a].getElementsByTagName("td");
     for (var b = 0; b < child_td.length; b++) {
       if (child_td[b].getElementsByTagName("button") != null) {
-        var btn__ = child_td[b].getElementById("install_id");
-        btn__.innerHTML = i18next.t('install_btn');
+        var btn__ = child_td[b].getElementsByTagName("button");
+        btn__[0].innerHTML = i18next.t('install_btn');
       }
     }
   }
