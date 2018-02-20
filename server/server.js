0ce6afec4fec59a60ea183fc8554c17c939d354f
zxcv1246789 <zxcv1246789@naver.com>
2018-02-21 00:37:17 +0900

    test

--- server/server.js
+++ server/server.js
@@ -3,7 +3,8 @@ module.exports = function(app, fs, url) {
 
   app.set('views', [
     __dirname + '/index',
-    __dirname + '/package/dhcpserver',
+    __dirname + '/package/networking',
+    __dirname + '/package/wpaconfig',
     __dirname + '/package/dashboard',
     __dirname + '/package/system',
   ]);
