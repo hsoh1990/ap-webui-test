e7740c23cd1a877c6d515bac698a7cab5c7fd1cf
zxcv1246789 <zxcv1246789@naver.com>
2018-02-21 13:41:24 +0900

    test

--- server/package_set.js
+++ server/package_set.js
@@ -1,8 +1,6 @@
 module.exports = function(app, fs, url) {
 
   require('./index/main.js')(app, fs, url);
-  require('./package/networking/main.js')(app, fs, url);
-  require('./package/wpaconfig/main.js')(app, fs, url);
   require('./package/dashboard/main.js')(app, fs, url);
   require('./package/system/main.js')(app, fs, url);
 
--- server/server.js
+++ server/server.js
@@ -3,8 +3,6 @@ module.exports = function(app, fs, url) {
 
   app.set('views', [
     __dirname + '/index',
-    __dirname + '/package/networking',
-    __dirname + '/package/wpaconfig',
     __dirname + '/package/dashboard',
     __dirname + '/package/system',
   ]);
