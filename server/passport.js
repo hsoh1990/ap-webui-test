ded6238fa447bc7c44db263d568efd4651d5f704
zxcv1246789 <zxcv1246789@naver.com>
2018-03-08 13:16:42 +0900

    test

--- server/passport.js
+++ server/passport.js
@@ -58,7 +58,7 @@ module.exports = (passport) => {
   passport.use(new GoogleStrategy({
     clientID: '93407170622-6aj2r2k85m4td8hk2jf250h96tv0asac.apps.googleusercontent.com',
     clientSecret: 'jayLRcvfHCrirMwbpuGrnDs4',
-    callbackURL: 'http://172.16.171.181/google/callback'
+    callbackURL: '/google/callback'
   }, function(accessToken, refreshToken, profile, done) {
     /*const socialId = profile.id;
     const nickname = profile.displayName;
