6a480fe0a4793ebaaf260d83f5ab22764fc8f95e
zxcv1246789 <zxcv1246789@naver.com>
2018-05-08 09:29:18 +0900

    test

--- server/public/index_2D/index_container.js
+++ server/public/index_2D/index_container.js
@@ -7,8 +7,8 @@ var stage = new Konva.Stage({
   height: stageHeight,
   scaleX: 0.57,
   scaleY: 0.57,
-  offsetX: -150,
-  offsetY: -50,
+  offsetX: -300,
+  offsetY: -150,
   draggable: true
 });
 
