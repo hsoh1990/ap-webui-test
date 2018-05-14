b426bfa92c3e279e03b31309b5da9b8cea012d3a
zxcv1246789 <zxcv1246789@naver.com>
2018-05-14 15:55:57 +0900

    test

--- server/index/js/index_login.js
+++ server/index/js/index_login.js
@@ -6,9 +6,6 @@ function init_() {
   socket = io.connect('http://172.16.171.181:8080');
   //connection_text(10, 1);
   //전역변수 선언
-  connect_count = 0;
-  disconnect_count = 0;
-  device_count = 0;
   connect_data = new Array();
   disconnect_data = new Array();
   ap_data = new Array();
@@ -113,7 +110,6 @@ function socket_event_device_data(data, type) {
     disconnect_data.splice(0, disconnect_data.length); //배열을 비운다.
   }
   devices_data = data;
-  device_count = devices_data.length;
   for (let a = 0; a < devices_data.length; a++) {
     if (devices_data[a]['arp'] == 1) {
       connect_data.push(devices_data[a]);
