db33c069a9614709741d5be87db26bbb1eb8a3a0
zxcv1246789 <zxcv1246789@naver.com>
2018-07-19 17:24:52 +0900

    test

--- server/install/test.sh
+++ server/install/test.sh
@@ -97,16 +97,17 @@ touch /etc/hostapd/hostapd.conf
 # 입맛대로 설정
 echo "interface=wlan0" | sudo tee -a /etc/hostapd/hostapd.conf
 echo "driver=nl80211" | sudo tee -a /etc/hostapd/hostapd.conf
-echo "ssid=testAP182" | sudo tee -a /etc/hostapd/hostapd.conf
+echo "ssid=testAP103" | sudo tee -a /etc/hostapd/hostapd.conf
 echo "hw_mode=g" | sudo tee -a /etc/hostapd/hostapd.conf
 echo "channel=6" | sudo tee -a /etc/hostapd/hostapd.conf
+echo "ieee80211n=1" | sudo tee -a /etc/hostapd/hostapd.conf
 echo "wmm_enabled=0" | sudo tee -a /etc/hostapd/hostapd.conf
+echo "ht_capab=[HT40][SHORT-GI-20][DSSS_CCK-40]" | sudo tee -a /etc/hostapd/hostapd.conf
 echo "macaddr_acl=0" | sudo tee -a /etc/hostapd/hostapd.conf
 echo "auth_algs=1" | sudo tee -a /etc/hostapd/hostapd.conf
 echo "wpa=2" | sudo tee -a /etc/hostapd/hostapd.conf
-echo "wpa_passphrase=0000000001" | sudo tee -a /etc/hostapd/hostapd.conf
+echo "wpa_passphrase=123412341234" | sudo tee -a /etc/hostapd/hostapd.conf
 echo "wpa_key_mgmt=WPA-PSK" | sudo tee -a /etc/hostapd/hostapd.conf
-echo "wpa_pairwise=TKIP" | sudo tee -a /etc/hostapd/hostapd.conf
 echo "rsn_pairwise=CCMP" | sudo tee -a /etc/hostapd/hostapd.conf
 
 mv /etc/default/hostapd /etc/default/hostapd.orig
@@ -120,12 +121,12 @@ mv /etc/dnsmasq.conf /etc/dnsmasq.conf.orig
 touch /etc/dnsmasq.conf
 # dhcp ip 입맛대로 설정
 echo "interface=wlan0" | sudo tee -a /etc/dnsmasq.conf
-echo "listen-address=192.168.10.1" | sudo tee -a /etc/dnsmasq.conf
 echo "bind-interfaces" | sudo tee -a /etc/dnsmasq.conf
 echo "server=8.8.8.8" | sudo tee -a /etc/dnsmasq.conf
 echo "domain-needed" | sudo tee -a /etc/dnsmasq.conf
 echo "bogus-priv" | sudo tee -a /etc/dnsmasq.conf
-echo "dhcp-range=192.168.10.12,192.168.10.200,12h" | sudo tee -a /etc/dnsmasq.conf
+echo "no-resolv" | sudo tee -a /etc/dnsmasq.conf
+echo "dhcp-range=192.168.10.12, 192.168.10.200, 12h" | sudo tee -a /etc/dnsmasq.conf
 
 echo "interfaces 설정"
 
@@ -138,16 +139,6 @@ echo "" | sudo tee -a /etc/network/interfaces
 echo "allow-hotplug wlan0" | sudo tee -a /etc/network/interfaces
 echo "iface wlan0 inet manual" | sudo tee -a /etc/network/interfaces
 echo "#wpa-conf /etc/wpa_supplicant/wpa_supplicant.conf" | sudo tee -a /etc/network/interfaces
-
-echo "rc.local 설정"
-
-mv /etc/rc.local /etc/rc.local.orig
-
-mv ap-webui-test-master/server/install/rc.local /etc/rc.local
-
-chmod +x /etc/rc.local
-
-echo "rc.local 설정 완료"
 
 echo "ipv4.ip_forward 설정"
 
@@ -167,15 +158,25 @@ iptables -A FORWARD -i wlan0 -o eth0 -j ACCEPT
 
 sh -c "iptables-save > /etc/iptables.ipv4.nat"
 
+echo "rc.local 설정"
+
+mv /etc/rc.local /etc/rc.local.orig
+
+mv ap-webui-test-master/server/install/rc.local /etc/rc.local
+
+chmod +x /etc/rc.local
+
+echo "rc.local 설정 완료"
+
 echo "hostapd, dnsmasq 서비스 시작"
 
-systemctl enable hostapd
-
-systemctl start hostapd
-
-systemctl enable dnsmasq
-
-systemctl start dnsmasq
+# systemctl enable hostapd
+#
+# systemctl start hostapd
+#
+# systemctl enable dnsmasq
+#
+# systemctl start dnsmasq
 
 echo "2. nodejs 설치"
 
@@ -185,7 +186,7 @@ apt-get autoremove -y
 
 curl -sL https://deb.nodesource.com/setup_8.x | bash -
 
-apt-get install nodejs -y
+apt-get install -y nodejs
 
 npm install nodemon -g
 
